4511e18be866711e25b7fc6b80ba218fcf722d5b
Prathibha Ratnayake <prsthibha@gmail.com>
2023-02-06 20:59:27 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import ListHeader from './components/ListHeader'
+
+function App() {
+  return (
+    <div className="App">
+      <ListHeader listName = '🏝️Holiday check list'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ListHeader.jsx
@@ -0,0 +1,8 @@
+
+const ListHeader = ({ listName }) => {
+  return (
+    <h1>{listName}</h1>
+  )
+}
+
+export default ListHeader
